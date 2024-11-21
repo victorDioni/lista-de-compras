7bea58203a32da2a783764386122134ae6dcb733
victorDioni <victor.dionizio28@gmail.com>
2024-11-21 16:42:29 -0300

    Refatorando a viewModel para usar o BD

--- app/src/main/java/com/victordionizio/listadecompras/ItemsViewModel.kt
+++ app/src/main/java/com/victordionizio/listadecompras/ItemsViewModel.kt
@@ -2,22 +2,33 @@ package com.victordionizio.listadecompras
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.victordionizio.listadecompras.data.ItemsDatabase
+import com.victordionizio.listadecompras.data.toModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ItemsViewModel(private val database: ItemsDatabase) : ViewModel() {
 
-    private var items = mutableListOf<ItemModel>()
     val itemsLiveData = MutableLiveData<List<ItemModel>>()
 
-    fun addItem(name : String){
-        val item = ItemModel(name = name, onRemove = :: removeItem)
-        items.add(item)
-        itemsLiveData.value = items
+    // Quando o aplicativo for aberto, acessará o BD e notificá a UI com todos os registros encontrados
+    init {
+        viewModelScope.launch (Dispatchers.IO){
+            fetchAll()
+        }
     }
 
-    private fun removeItem(item: ItemModel){
-        items.remove(item)
-        itemsLiveData.value = items
+    fun addItem(name : String){
+
+    }
+
+    private suspend fun fetchAll(){
+        val result = database.itemsDao().getAll().map {
+            it.toModel { onRemove = {} }
+        }
+
+        itemsLiveData.postValue(result)
     }
 
 }
